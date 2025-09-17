35db73bf85b63efac3ce3840537531d4e8591987
fmtab <fmtab2014@gmail.com>
2025-09-17 14:52:37 +0500

    try ip

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -90,7 +90,8 @@ public:
             m_cv.notify_one();
             };
         // 92.255.165.77
-        m_client = std::make_unique<CallsClient>("192.168.56.1",
+        // 192.168.1.45
+        m_client = std::make_unique<CallsClient>("92.255.165.77",
             authCallback,
             createCallCallback,
             incomingCallCallback,
